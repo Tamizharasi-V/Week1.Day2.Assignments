db3fc5e756c522116ee20317ed0855cdaa96a643
Tamizharasi-V <tamizharasi0113@gmail.com>
2025-11-09 14:25:02 +0530

    Add files via upload
    Week 1 Day 2 Assignments

---
+++ FindOddNumbers.java
@@ -0,0 +1,14 @@
+package week1.day2assignments;
+
+public class FindOddNumbers {
+public static void main(String[] args) {
+	int maxRange = 10;
+	System.out.println("Odd Numbers within the max range of 10 is :");
+	for (int i = 1; i <= maxRange; i++) {
+	if (i%2!=0)	
+	{
+		System.out.println(i);
+	}
+}
+}
+}
